bd0e410be883cc615c472f8f027ee3f66c04ee91
unknown <rifkymuhamad0022@gmail.com>
2023-12-17 16:16:21 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+import express from "express";
+import dotenv from "dotenv";
+import winston from "winston";
+
+// configuration .env
+dotenv.config();
+
+// access PORT key
+const port = process.env.PORT;
+
+// create application
+const app = express();
+
+// create logger
+const logger = winston.createLogger({});
+
+app.get("/", (req, res) => {
+  res.send("Hello, Express!");
+});
+
+app.listen(
+  port,
+  logger.log({
+    level: "info",
+    message: "Server is running",
+  })
+);
