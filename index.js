22e56e0a1e7d2beadaaa2557f8c63b3659ab5907
unknown <rifkymuhamad0022@gmail.com>
2024-01-05 18:19:03 +0700

    deploy vercel

--- index.js
+++ index.js
@@ -1,24 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
-import cors from "cors";
 import { logger } from "./log/log.js";
 import router from "./routes/routes.js";
 
 dotenv.config();
 
 const port = process.env.PORT;
-const portOrigin = process.env.PORTORIGIN;
 
 const app = express();
-
-const middlewares = [
-  cors({ credentials: true, origin: portOrigin }),
-  express.json(),
-];
-
-for (const middleware of middlewares) {
-  app.use(middleware);
-}
 
 app.use(router);
 
