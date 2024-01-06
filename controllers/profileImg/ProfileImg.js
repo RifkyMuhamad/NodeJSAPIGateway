e832da35f6cf09b7dce7a269a6a72303ab592a58
unknown <rifkymuhamad0022@gmail.com>
2024-01-06 17:32:15 +0700

    Create ProfileImg.js

---
+++ controllers/profileImg/ProfileImg.js
@@ -0,0 +1,21 @@
+import fs from "fs";
+
+export const ProfileImage = async (req, res) => {
+    try {
+        // Baca file gambar secara asinkron
+        const imageBuffer = await fs.promises.readFile(
+          "./assets/python_api_gateway_card.jpg"
+        );
+    
+        // Konversi buffer ke base64 untuk menyertakan gambar dalam respons JSON
+        const base64Image = imageBuffer.toString('base64');
+    
+        res.json({
+          message: base64Image,
+        });
+      } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+      }
+  };
+  
--- routes/routes.js
+++ routes/routes.js
@@ -4,6 +4,7 @@ import { about, aboutId } from "../controllers/about.js";
 import { languagesList } from "../controllers/languages.js";
 import { profileTitle } from "../controllers/ProfileTitle.js";
 import { navbarItems } from "../controllers/NavbarItems.js";
+import { ProfileImage } from "../controllers/profileImg/ProfileImg.js";
 
 const router = express.Router();
 
@@ -12,6 +13,7 @@ router.get("/about", about);
 router.get("/about/:id", aboutId);
 router.get("/navbar/items", navbarItems);
 router.get("/profile/titles", profileTitle);
+router.get("/profile/image/python-api-gateway", ProfileImage);
 router.get("/foreign-lang-list", languagesList);
 
 export default router;
