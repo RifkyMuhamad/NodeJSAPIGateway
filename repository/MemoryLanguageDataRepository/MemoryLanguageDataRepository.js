9c61ab643a003cf3ed70f4fad3d36fc794213c45
DyoneStrankers <rifkymuhamad0022@gmail.com>
2024-03-08 09:00:52 +0700

    create /project/memory-language/data route:fix bugV2

--- repository/MemoryLanguageDataRepository/MemoryLanguageDataRepository.js
+++ repository/MemoryLanguageDataRepository/MemoryLanguageDataRepository.js
@@ -1,11 +1,20 @@
 import mongoose from "mongoose";
 import {collectionNames} from "../../JSON/CollectionNames.js";
+import {logger} from "../../log/log.js";
 
 async function get() {
-    const collections = await mongoose.connection.db.listCollections().toArray();
-    return collections.map(collection => {
-        return collectionNames[collection.name] || collection.name;
-    });
+    try {
+        const collections = await mongoose.connection.db.listCollections().toArray();
+        return collections.map(collection => {
+            return collectionNames[collection.name] || collection.name;
+        });
+    } catch (error) {
+        logger.log({
+            level: "error",
+            message: `Terjadi kesalahan ${ error }`,
+        })
+        return "Kodenya Error";
+    }
 }
 
 export default { get };
